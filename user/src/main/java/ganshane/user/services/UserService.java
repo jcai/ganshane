c1528a41a524f23296485787ee11a32039e6cc04
Jun Tsai <jun.tsai@gmail.com>
2010-06-03 20:42:04 +0800

    增加用户服务类

---
+++ user/src/main/java/ganshane/user/services/UserService.java
@@ -0,0 +1,34 @@
+/* 
+ * Copyright 2010 The Ganshane Team.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ganshane.user.services;
+
+import ganshane.user.entities.User;
+
+/**
+ * 用户相关的服务类
+ * @author <a href="mailto:jun.tsai@gmail.com">Jun Tsai</a>
+ * @version $Revision$
+ * @since 0.1
+ */
+public interface UserService {
+    /**
+     * 注册新用户
+     * @return 注册之后的用户试题
+     * @since 0.1
+     */
+    public User register();
+    public User login(String userName,String password);
+}
